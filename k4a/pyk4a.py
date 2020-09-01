8812509efb0e6577a87d619a7e13f70f1414aa33
gmorell <gmp@gmp.io>
2020-08-31 20:05:51 -0400

    more ctypes

--- k4a/pyk4a.py
+++ k4a/pyk4a.py
@@ -6,54 +6,66 @@ try:
     _k4a = ctypes.CDLL(r'C:\Program Files\Azure Kinect SDK v1.3.0\sdk\windows-desktop\amd64\release\bin\k4a.dll')
 except Exception as e:
     try:
-        _k4a = ctypes.CDLL('k4a.so')
+        _k4a = ctypes.CDLL('libk4a.so')
     except Exception as ee:
         print("Failed to load library", e, ee)
         sys.exit(1)
 
+
 # K4A_DECLARE_HANDLE(k4a_device_t);
 class _handle_k4a_device_t(ctypes.Structure):
-     _fields_= [
+    _fields_ = [
         ("_rsvd", ctypes.c_size_t),
     ]
+
+
 k4a_device_t = ctypes.POINTER(_handle_k4a_device_t)
+
 
 # K4A_DECLARE_HANDLE(k4a_capture_t);
 class _handle_k4a_capture_t(ctypes.Structure):
-     _fields_= [
+    _fields_ = [
         ("_rsvd", ctypes.c_size_t),
     ]
+
+
 k4a_capture_t = ctypes.POINTER(_handle_k4a_capture_t)
+
 
 # K4A_DECLARE_HANDLE(k4a_image_t);
 class _handle_k4a_image_t(ctypes.Structure):
-     _fields_= [
+    _fields_ = [
         ("_rsvd", ctypes.c_size_t),
     ]
+
+
 k4a_image_t = ctypes.POINTER(_handle_k4a_image_t)
+
 
 # K4A_DECLARE_HANDLE(k4a_transformation_t);
 class _handle_k4a_transformation_t(ctypes.Structure):
-     _fields_= [
+    _fields_ = [
         ("_rsvd", ctypes.c_size_t),
     ]
+
+
 k4a_transformation_t = ctypes.POINTER(_handle_k4a_transformation_t)
 
-#class k4a_result_t(CtypeIntEnum):
+# class k4a_result_t(CtypeIntEnum):
 K4A_RESULT_SUCCEEDED = 0
 K4A_RESULT_FAILED = 1
 
-#class k4a_buffer_result_t(CtypeIntEnum):
+# class k4a_buffer_result_t(CtypeIntEnum):
 K4A_BUFFER_RESULT_SUCCEEDED = 0
 K4A_BUFFER_RESULT_FAILED = 1
 K4A_BUFFER_RESULT_TOO_SMALL = 2
 
-#class k4a_wait_result_t(CtypeIntEnum):
+# class k4a_wait_result_t(CtypeIntEnum):
 K4A_WAIT_RESULT_SUCCEEDED = 0
 K4A_WAIT_RESULT_FAILED = 1
 K4A_WAIT_RESULT_TIMEOUT = 2
 
-#class k4a_log_level_t(CtypeIntEnum):
+# class k4a_log_level_t(CtypeIntEnum):
 K4A_LOG_LEVEL_CRITICAL = 0
 K4A_LOG_LEVEL_ERROR = 1
 K4A_LOG_LEVEL_WARNING = 2
@@ -61,7 +73,7 @@ K4A_LOG_LEVEL_INFO = 3
 K4A_LOG_LEVEL_TRACE = 4
 K4A_LOG_LEVEL_OFF = 5
 
-#class k4a_depth_mode_t(CtypeIntEnum):
+# class k4a_depth_mode_t(CtypeIntEnum):
 K4A_DEPTH_MODE_OFF = 0
 K4A_DEPTH_MODE_NFOV_2X2BINNED = 1
 K4A_DEPTH_MODE_NFOV_UNBINNED = 2
@@ -69,7 +81,7 @@ K4A_DEPTH_MODE_WFOV_2X2BINNED = 3
 K4A_DEPTH_MODE_WFOV_UNBINNED = 4
 K4A_DEPTH_MODE_PASSIVE_IR = 5
 
-#class k4a_color_resolution_t(CtypeIntEnum):
+# class k4a_color_resolution_t(CtypeIntEnum):
 K4A_COLOR_RESOLUTION_OFF = 0
 K4A_COLOR_RESOLUTION_720P = 1
 K4A_COLOR_RESOLUTION_1080P = 2
@@ -78,7 +90,7 @@ K4A_COLOR_RESOLUTION_1536P = 4
 K4A_COLOR_RESOLUTION_2160P = 5
 K4A_COLOR_RESOLUTION_3072P = 6
 
-#class k4a_image_format_t(CtypeIntEnum):
+# class k4a_image_format_t(CtypeIntEnum):
 K4A_IMAGE_FORMAT_COLOR_MJPG = 0
 K4A_IMAGE_FORMAT_COLOR_NV12 = 1
 K4A_IMAGE_FORMAT_COLOR_YUY2 = 2
@@ -89,16 +101,16 @@ K4A_IMAGE_FORMAT_CUSTOM8 = 6
 K4A_IMAGE_FORMAT_CUSTOM16 = 7
 K4A_IMAGE_FORMAT_CUSTOM = 8
 
-#class k4a_transformation_interpolation_type_t(CtypeIntEnum):
+# class k4a_transformation_interpolation_type_t(CtypeIntEnum):
 K4A_TRANSFORMATION_INTERPOLATION_TYPE_NEAREST = 0
 K4A_TRANSFORMATION_INTERPOLATION_TYPE_LINEAR = 1
 
-#class k4a_fps_t(CtypeIntEnum):
+# class k4a_fps_t(CtypeIntEnum):
 K4A_FRAMES_PER_SECOND_5 = 0
 K4A_FRAMES_PER_SECOND_15 = 1
 K4A_FRAMES_PER_SECOND_30 = 2
 
-#class k4a_color_control_command_t(CtypeIntEnum):
+# class k4a_color_control_command_t(CtypeIntEnum):
 K4A_COLOR_CONTROL_EXPOSURE_TIME_ABSOLUTE = 0
 K4A_COLOR_CONTROL_AUTO_EXPOSURE_PRIORITY = 1
 K4A_COLOR_CONTROL_BRIGHTNESS = 2
@@ -110,17 +122,16 @@ K4A_COLOR_CONTROL_BACKLIGHT_COMPENSATION = 7
 K4A_COLOR_CONTROL_GAIN = 8
 K4A_COLOR_CONTROL_POWERLINE_FREQUENCY = 9
 
-#class k4a_color_control_mode_t(CtypeIntEnum):
+# class k4a_color_control_mode_t(CtypeIntEnum):
 K4A_COLOR_CONTROL_MODE_AUTO = 0
 K4A_COLOR_CONTROL_MODE_MANUAL = 1
 
-    
-#class k4a_wired_sync_mode_t(CtypeIntEnum):
+# class k4a_wired_sync_mode_t(CtypeIntEnum):
 K4A_WIRED_SYNC_MODE_STANDALONE = 0
 K4A_WIRED_SYNC_MODE_MASTER = 1
 K4A_WIRED_SYNC_MODE_SUBORDINATE = 2
 
-#class k4a_calibration_type_t(CtypeIntEnum):
+# class k4a_calibration_type_t(CtypeIntEnum):
 K4A_CALIBRATION_TYPE_UNKNOWN = -1
 K4A_CALIBRATION_TYPE_DEPTH = 0
 K4A_CALIBRATION_TYPE_COLOR = 1
@@ -128,29 +139,32 @@ K4A_CALIBRATION_TYPE_GYRO = 2
 K4A_CALIBRATION_TYPE_ACCEL = 3
 K4A_CALIBRATION_TYPE_NUM = 4
 
-#class k4a_calibration_model_type_t(CtypeIntEnum):
+# class k4a_calibration_model_type_t(CtypeIntEnum):
 K4A_CALIBRATION_LENS_DISTORTION_MODEL_UNKNOWN = 0
 K4A_CALIBRATION_LENS_DISTORTION_MODEL_THETA = 1
 K4A_CALIBRATION_LENS_DISTORTION_MODEL_POLYNOMIAL_3K = 2
 K4A_CALIBRATION_LENS_DISTORTION_MODEL_RATIONAL_6KT = 3
 K4A_CALIBRATION_LENS_DISTORTION_MODEL_BROWN_CONRADY = 4
 
-#class k4a_firmware_build_t(CtypeIntEnum):
+# class k4a_firmware_build_t(CtypeIntEnum):
 K4A_FIRMWARE_BUILD_RELEASE = 0
 K4A_FIRMWARE_BUILD_DEBUG = 1
 
-#class k4a_firmware_signature_t(CtypeIntEnum):
+# class k4a_firmware_signature_t(CtypeIntEnum):
 K4A_FIRMWARE_SIGNATURE_MSFT = 0
 K4A_FIRMWARE_SIGNATURE_TEST = 1
 K4A_FIRMWARE_SIGNATURE_UNSIGNED = 2
 
-#define K4A_SUCCEEDED(_result_) (_result_ == K4A_RESULT_SUCCEEDED)
+
+# define K4A_SUCCEEDED(_result_) (_result_ == K4A_RESULT_SUCCEEDED)
 def K4A_SUCCEEDED(result):
     return result == K4A_RESULT_SUCCEEDED
 
-#define K4A_FAILED(_result_) (!K4A_SUCCEEDED(_result_))
+
+# define K4A_FAILED(_result_) (!K4A_SUCCEEDED(_result_))
 def K4A_FAILED(result):
     return not K4A_SUCCEEDED(result)
+
 
 # TODO(Andoryuuta): Callbacks, are these needed?
 """
@@ -164,8 +178,9 @@ typedef void(k4a_memory_destroy_cb_t)(void *buffer, void *context);
 typedef uint8_t *(k4a_memory_allocate_cb_t)(int size, void **context);
 """
 
+
 class _k4a_device_configuration_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("color_format", ctypes.c_int),
         ("color_resolution", ctypes.c_int),
         ("depth_mode", ctypes.c_int),
@@ -177,14 +192,19 @@ class _k4a_device_configuration_t(ctypes.Structure):
         ("disable_streaming_indicator", ctypes.c_bool),
     ]
 
+
 k4a_device_configuration_t = _k4a_device_configuration_t
 
+
 class _k4a_calibration_extrinsics_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("rotation", ctypes.c_float * 9),
         ("translation", ctypes.c_float * 3),
     ]
+
+
 k4a_calibration_extrinsics_t = _k4a_calibration_extrinsics_t
+
 
 class _param(ctypes.Structure):
     _fields_ = [
@@ -205,52 +225,64 @@ class _param(ctypes.Structure):
         ("metric_radius", ctypes.c_float),
     ]
 
+
 class k4a_calibration_intrinsic_parameters_t(ctypes.Union):
-    _fields_= [
+    _fields_ = [
         ("param", _param),
         ("v", ctypes.c_float * 15),
     ]
 
+
 class _k4a_calibration_intrinsics_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("type", ctypes.c_int),
         ("parameter_count", ctypes.c_uint),
         ("parameters", k4a_calibration_intrinsic_parameters_t),
     ]
 
+
 k4a_calibration_intrinsics_t = _k4a_calibration_intrinsics_t
 
+
 class _k4a_calibration_camera_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("extrinsics", k4a_calibration_extrinsics_t),
         ("intrinsics", k4a_calibration_intrinsics_t),
         ("resolution_width", ctypes.c_int),
         ("resolution_height", ctypes.c_int),
         ("metric_radius", ctypes.c_float),
     ]
+
+
 k4a_calibration_camera_t = _k4a_calibration_camera_t
 
+
 class _k4a_calibration_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("depth_camera_calibration", k4a_calibration_camera_t),
         ("color_camera_calibration", k4a_calibration_camera_t),
         ("extrinsics", (k4a_calibration_extrinsics_t * K4A_CALIBRATION_TYPE_NUM) * K4A_CALIBRATION_TYPE_NUM),
         ("depth_mode", ctypes.c_int),
         ("color_resolution", ctypes.c_int),
     ]
+
+
 k4a_calibration_t = _k4a_calibration_t
 
+
 class _k4a_version_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("major", ctypes.c_uint32),
         ("minor", ctypes.c_uint32),
         ("iteration", ctypes.c_uint32),
     ]
+
+
 k4a_version_t = _k4a_version_t
 
 
 class _k4a_hardware_version_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("rgb", k4a_version_t),
         ("depth", k4a_version_t),
         ("audio", k4a_version_t),
@@ -258,41 +290,50 @@ class _k4a_hardware_version_t(ctypes.Structure):
         ("firmware_build", ctypes.c_int),
         ("firmware_signature", ctypes.c_int),
     ]
+
+
 k4a_hardware_version_t = _k4a_hardware_version_t
 
+
 class _xy(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("x", ctypes.c_float),
         ("y", ctypes.c_float),
     ]
 
+
 class k4a_float2_t(ctypes.Union):
-   _fields_= [
+    _fields_ = [
         ("xy", _xy),
         ("v", ctypes.c_float * 2)
     ]
 
+
 class _xyz(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("x", ctypes.c_float),
         ("y", ctypes.c_float),
         ("z", ctypes.c_float),
     ]
 
+
 class k4a_float3_t(ctypes.Union):
-   _fields_= [
+    _fields_ = [
         ("xyz", _xyz),
         ("v", ctypes.c_float * 3)
     ]
 
+
 class _k4a_imu_sample_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("temperature", ctypes.c_float),
         ("acc_sample", k4a_float3_t),
         ("acc_timestamp_usec", ctypes.c_uint64),
         ("gyro_sample", k4a_float3_t),
         ("gyro_timestamp_usec", ctypes.c_uint64),
     ]
+
+
 k4a_imu_sample_t = _k4a_imu_sample_t
 
 K4A_DEVICE_DEFAULT = 0
@@ -311,15 +352,15 @@ K4A_DEVICE_CONFIG_INIT_DISABLE_ALL.subordinate_delay_off_master_usec = 0
 K4A_DEVICE_CONFIG_INIT_DISABLE_ALL.disable_streaming_indicator = False
 
 # Functions
-#K4A_EXPORT k4a_result_t k4a_device_open(uint32_t index, k4a_device_t *device_handle);
+# K4A_EXPORT k4a_result_t k4a_device_open(uint32_t index, k4a_device_t *device_handle);
 k4a_device_open = _k4a.k4a_device_open
-k4a_device_open.restype=ctypes.c_int
-k4a_device_open.argtypes=(ctypes.c_uint32, ctypes.POINTER(k4a_device_t))
+k4a_device_open.restype = ctypes.c_int
+k4a_device_open.argtypes = (ctypes.c_uint32, ctypes.POINTER(k4a_device_t))
 
-#K4A_EXPORT k4a_result_t k4a_device_start_cameras(k4a_device_t device_handle, const k4a_device_configuration_t *config);
+# K4A_EXPORT k4a_result_t k4a_device_start_cameras(k4a_device_t device_handle, const k4a_device_configuration_t *config);
 k4a_device_start_cameras = _k4a.k4a_device_start_cameras
-k4a_device_start_cameras.restype=ctypes.c_int
-k4a_device_start_cameras.argtypes=(k4a_device_t, ctypes.POINTER(k4a_device_configuration_t))
+k4a_device_start_cameras.restype = ctypes.c_int
+k4a_device_start_cameras.argtypes = (k4a_device_t, ctypes.POINTER(k4a_device_configuration_t))
 
 """
 K4A_EXPORT k4a_result_t k4a_device_get_calibration(k4a_device_t device_handle,
@@ -328,8 +369,8 @@ K4A_EXPORT k4a_result_t k4a_device_get_calibration(k4a_device_t device_handle,
                                                    k4a_calibration_t *calibration);
 """
 k4a_device_get_calibration = _k4a.k4a_device_get_calibration
-k4a_device_get_calibration.restype=ctypes.c_int
-k4a_device_get_calibration.argtypes=(k4a_device_t, ctypes.c_int, ctypes.c_int, ctypes.POINTER(k4a_calibration_t))
+k4a_device_get_calibration.restype = ctypes.c_int
+k4a_device_get_calibration.argtypes = (k4a_device_t, ctypes.c_int, ctypes.c_int, ctypes.POINTER(k4a_calibration_t))
 
 """
 K4A_EXPORT k4a_wait_result_t k4a_device_get_capture(k4a_device_t device_handle,
@@ -337,21 +378,47 @@ K4A_EXPORT k4a_wait_result_t k4a_device_get_capture(k4a_device_t device_handle,
                                                     int32_t timeout_in_ms);
 """
 k4a_device_get_capture = _k4a.k4a_device_get_capture
-k4a_device_get_capture.restype=ctypes.c_int
-k4a_device_get_capture.argtypes=(k4a_device_t, ctypes.POINTER(k4a_capture_t), ctypes.c_int32)
+k4a_device_get_capture.restype = ctypes.c_int
+k4a_device_get_capture.argtypes = (k4a_device_t, ctypes.POINTER(k4a_capture_t), ctypes.c_int32)
 
-#K4A_EXPORT void k4a_capture_release(k4a_capture_t capture_handle);
+# K4A_EXPORT void k4a_capture_release(k4a_capture_t capture_handle);
 k4a_capture_release = _k4a.k4a_capture_release
-k4a_capture_release.argtypes=(k4a_capture_t,)
+k4a_capture_release.argtypes = (k4a_capture_t,)
 
-#K4A_EXPORT void k4a_image_release(k4a_image_t image_handle);
+# K4A_EXPORT void k4a_image_release(k4a_image_t image_handle);
 k4a_image_release = _k4a.k4a_image_release
-k4a_image_release.argtypes=(k4a_image_t,)
+k4a_image_release.argtypes = (k4a_image_t,)
 
-#K4A_EXPORT void k4a_device_stop_cameras(k4a_device_t device_handle);
+# K4A_EXPORT void k4a_device_stop_cameras(k4a_device_t device_handle);
 k4a_device_stop_cameras = _k4a.k4a_device_stop_cameras
-k4a_device_stop_cameras.argtypes=(k4a_device_t,)
+k4a_device_stop_cameras.argtypes = (k4a_device_t,)
 
-#K4A_EXPORT void k4a_device_close(k4a_device_t device_handle);
+# K4A_EXPORT void k4a_device_close(k4a_device_t device_handle);
 k4a_device_close = _k4a.k4a_device_close
-k4a_device_close.argtypes=(k4a_device_t,)
+k4a_device_close.argtypes = (k4a_device_t,)
+
+c_uint8_p = ctypes.POINTER(ctypes.c_uint8)
+
+k4a_image_get_buffer = _k4a.k4a_image_get_buffer
+k4a_image_get_buffer.restype = c_uint8_p
+k4a_image_get_buffer.argtypes = (k4a_image_t,)
+
+k4a_image_get_height_pixels = _k4a.k4a_image_get_height_pixels
+k4a_image_get_height_pixels.restype = ctypes.c_int
+k4a_image_get_height_pixels.argtypes = (k4a_image_t,)
+
+k4a_image_get_width_pixels = _k4a.k4a_image_get_width_pixels
+k4a_image_get_width_pixels.restype = ctypes.c_int
+k4a_image_get_width_pixels.argtypes = (k4a_image_t,)
+
+k4a_capture_get_color_image = _k4a.k4a_capture_get_color_image
+k4a_capture_get_color_image.restype = k4a_image_t
+k4a_capture_get_color_image.argtypes = (k4a_capture_t,)
+
+k4a_capture_get_depth_image = _k4a.k4a_capture_get_depth_image
+k4a_capture_get_depth_image.restype = k4a_image_t
+k4a_capture_get_depth_image.argtypes = (k4a_capture_t,)
+
+k4a_capture_get_ir_image = _k4a.k4a_capture_get_ir_image
+k4a_capture_get_ir_image.restype = k4a_image_t
+k4a_capture_get_ir_image.argtypes = (k4a_capture_t,)
--- k4a/pyk4abt.py
+++ k4a/pyk4abt.py
@@ -5,29 +5,36 @@ import sys
 from .pyk4a import k4a_float3_t, k4a_calibration_t, k4a_capture_t, k4a_image_t
 
 try:
-    _k4abt = ctypes.CDLL(r'C:\Program Files\Azure Kinect Body Tracking SDK\sdk\windows-desktop\amd64\release\bin\k4abt.dll')
+    _k4abt = ctypes.CDLL(
+        r'C:\Program Files\Azure Kinect Body Tracking SDK\sdk\windows-desktop\amd64\release\bin\k4abt.dll')
 except Exception as e:
     try:
-        _k4abt = ctypes.CDLL('k4abt.so')
+        _k4abt = ctypes.CDLL('libk4abt.so')
     except Exception as ee:
         print("Failed to load library", e, ee)
         sys.exit(1)
 
+
 # K4A_DECLARE_HANDLE(k4abt_tracker_t);
 class _handle_k4abt_tracker_t(ctypes.Structure):
-     _fields_= [
+    _fields_ = [
         ("_rsvd", ctypes.c_size_t),
     ]
+
+
 k4abt_tracker_t = ctypes.POINTER(_handle_k4abt_tracker_t)
+
 
 # K4A_DECLARE_HANDLE(k4abt_frame_t);
 class _handle_k4abt_frame_t(ctypes.Structure):
-     _fields_= [
+    _fields_ = [
         ("_rsvd", ctypes.c_size_t),
     ]
+
+
 k4abt_frame_t = ctypes.POINTER(_handle_k4abt_frame_t)
 
-#class k4abt_joint_id_t(CtypeIntEnum):
+# class k4abt_joint_id_t(CtypeIntEnum):
 K4ABT_JOINT_PELVIS = 0
 K4ABT_JOINT_SPINE_NAVEL = 1
 K4ABT_JOINT_SPINE_CHEST = 2
@@ -62,39 +69,45 @@ K4ABT_JOINT_EYE_RIGHT = 30
 K4ABT_JOINT_EAR_RIGHT = 31
 K4ABT_JOINT_COUNT = 33
 
-#class k4abt_sensor_orientation_t(CtypeIntEnum):
+# class k4abt_sensor_orientation_t(CtypeIntEnum):
 K4ABT_SENSOR_ORIENTATION_DEFAULT = 0
 K4ABT_SENSOR_ORIENTATION_CLOCKWISE90 = 1
 K4ABT_SENSOR_ORIENTATION_COUNTERCLOCKWISE90 = 2
 K4ABT_SENSOR_ORIENTATION_FLIP180 = 3
 
-#class k4abt_tracker_processing_mode_t(CtypeIntEnum):
+# class k4abt_tracker_processing_mode_t(CtypeIntEnum):
 K4ABT_TRACKER_PROCESSING_MODE_GPU = 0
 K4ABT_TRACKER_PROCESSING_MODE_CPU = 1
 
+
 class _k4abt_tracker_configuration_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("sensor_orientation", ctypes.c_int),
         ("processing_mode", ctypes.c_int),
         ("gpu_device_id", ctypes.c_int32),
     ]
+
+
 k4abt_tracker_configuration_t = _k4abt_tracker_configuration_t
 
+
 class _wxyz(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("w", ctypes.c_float),
         ("x", ctypes.c_float),
         ("y", ctypes.c_float),
         ("z", ctypes.c_float),
     ]
 
+
 class k4a_quaternion_t(ctypes.Union):
-   _fields_= [
+    _fields_ = [
         ("wxyz", _wxyz),
         ("v", ctypes.c_float * 4)
     ]
 
-#class k4abt_joint_confidence_level_t(CtypeIntEnum):
+
+# class k4abt_joint_confidence_level_t(CtypeIntEnum):
 K4ABT_JOINT_CONFIDENCE_NONE = 0
 K4ABT_JOINT_CONFIDENCE_LOW = 1
 K4ABT_JOINT_CONFIDENCE_MEDIUM = 2
@@ -103,23 +116,28 @@ K4ABT_JOINT_CONFIDENCE_LEVELS_COUNT = 4
 
 
 class _k4abt_joint_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("position", k4a_float3_t),
         ("orientation", k4a_quaternion_t),
         ("confidence_level", ctypes.c_int),
     ]
+
+
 k4abt_joint_t = _k4abt_joint_t
 
+
 class k4abt_skeleton_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("joints", _k4abt_joint_t * K4ABT_JOINT_COUNT),
     ]
 
+
 class k4abt_body_t(ctypes.Structure):
-    _fields_= [
+    _fields_ = [
         ("id", ctypes.c_uint32),
         ("skeleton", k4abt_skeleton_t),
     ]
+
 
 K4ABT_BODY_INDEX_MAP_BACKGROUND = 255
 K4ABT_INVALID_BODY_ID = 0xFFFFFFFF
@@ -133,55 +151,56 @@ K4ABT_TRACKER_CONFIG_DEFAULT.gpu_device_id = 0
 
 # Functions
 k4abt_tracker_create = _k4abt.k4abt_tracker_create
-k4abt_tracker_create.restype=ctypes.c_int
-k4abt_tracker_create.argtypes=(ctypes.POINTER(k4a_calibration_t), k4abt_tracker_configuration_t, ctypes.POINTER(k4abt_tracker_t))
+k4abt_tracker_create.restype = ctypes.c_int
+k4abt_tracker_create.argtypes = (
+ctypes.POINTER(k4a_calibration_t), k4abt_tracker_configuration_t, ctypes.POINTER(k4abt_tracker_t))
 
 k4abt_tracker_destroy = _k4abt.k4abt_tracker_destroy
-k4abt_tracker_destroy.argtypes=(k4abt_tracker_t,)
+k4abt_tracker_destroy.argtypes = (k4abt_tracker_t,)
 
 k4abt_tracker_set_temporal_smoothing = _k4abt.k4abt_tracker_set_temporal_smoothing
-k4abt_tracker_set_temporal_smoothing.argtypes=(k4abt_tracker_t, ctypes.c_float)
+k4abt_tracker_set_temporal_smoothing.argtypes = (k4abt_tracker_t, ctypes.c_float)
 
 k4abt_tracker_enqueue_capture = _k4abt.k4abt_tracker_enqueue_capture
-k4abt_tracker_enqueue_capture.restype=ctypes.c_int
-k4abt_tracker_enqueue_capture.argtypes=(k4abt_tracker_t, k4a_capture_t, ctypes.c_int32)
+k4abt_tracker_enqueue_capture.restype = ctypes.c_int
+k4abt_tracker_enqueue_capture.argtypes = (k4abt_tracker_t, k4a_capture_t, ctypes.c_int32)
 
 k4abt_tracker_pop_result = _k4abt.k4abt_tracker_pop_result
-k4abt_tracker_pop_result.restype=ctypes.c_int
-k4abt_tracker_pop_result.argtypes=(k4abt_tracker_t, ctypes.POINTER(k4abt_frame_t), ctypes.c_int32)
+k4abt_tracker_pop_result.restype = ctypes.c_int
+k4abt_tracker_pop_result.argtypes = (k4abt_tracker_t, ctypes.POINTER(k4abt_frame_t), ctypes.c_int32)
 
 k4abt_tracker_shutdown = _k4abt.k4abt_tracker_shutdown
-k4abt_tracker_shutdown.argtypes=(k4abt_tracker_t,)
+k4abt_tracker_shutdown.argtypes = (k4abt_tracker_t,)
 
 k4abt_frame_release = _k4abt.k4abt_frame_release
-k4abt_frame_release.argtypes=(k4abt_frame_t,)
+k4abt_frame_release.argtypes = (k4abt_frame_t,)
 
 k4abt_frame_reference = _k4abt.k4abt_frame_reference
-k4abt_frame_reference.argtypes=(k4abt_frame_t,)
+k4abt_frame_reference.argtypes = (k4abt_frame_t,)
 
 k4abt_frame_get_num_bodies = _k4abt.k4abt_frame_get_num_bodies
-k4abt_frame_get_num_bodies.restype=ctypes.c_uint32
-k4abt_frame_get_num_bodies.argtypes=(k4abt_frame_t,)
+k4abt_frame_get_num_bodies.restype = ctypes.c_uint32
+k4abt_frame_get_num_bodies.argtypes = (k4abt_frame_t,)
 
 k4abt_frame_get_body_skeleton = _k4abt.k4abt_frame_get_body_skeleton
-k4abt_frame_get_body_skeleton.restype=ctypes.c_int
-k4abt_frame_get_body_skeleton.argtypes=(k4abt_frame_t, ctypes.c_uint32, ctypes.POINTER(k4abt_skeleton_t))
+k4abt_frame_get_body_skeleton.restype = ctypes.c_int
+k4abt_frame_get_body_skeleton.argtypes = (k4abt_frame_t, ctypes.c_uint32, ctypes.POINTER(k4abt_skeleton_t))
 
 k4abt_frame_get_body_id = _k4abt.k4abt_frame_get_body_id
-k4abt_frame_get_body_id.restype=ctypes.c_uint32
-k4abt_frame_get_body_id.argtypes=(k4abt_frame_t, ctypes.c_uint32)
+k4abt_frame_get_body_id.restype = ctypes.c_uint32
+k4abt_frame_get_body_id.argtypes = (k4abt_frame_t, ctypes.c_uint32)
 
 k4abt_frame_get_device_timestamp_usec = _k4abt.k4abt_frame_get_device_timestamp_usec
-k4abt_frame_get_device_timestamp_usec.restype=ctypes.c_uint64
-k4abt_frame_get_device_timestamp_usec.argtypes=(k4abt_frame_t,)
+k4abt_frame_get_device_timestamp_usec.restype = ctypes.c_uint64
+k4abt_frame_get_device_timestamp_usec.argtypes = (k4abt_frame_t,)
 
 k4abt_frame_get_body_index_map = _k4abt.k4abt_frame_get_body_index_map
-k4abt_frame_get_body_index_map.restype=k4a_image_t
-k4abt_frame_get_body_index_map.argtypes=(k4abt_frame_t,)
+k4abt_frame_get_body_index_map.restype = k4a_image_t
+k4abt_frame_get_body_index_map.argtypes = (k4abt_frame_t,)
 
 k4abt_frame_get_capture = _k4abt.k4abt_frame_get_capture
-k4abt_frame_get_capture.restype=k4a_capture_t
-k4abt_frame_get_capture.argtypes=(k4abt_frame_t,)
+k4abt_frame_get_capture.restype = k4a_capture_t
+k4abt_frame_get_capture.argtypes = (k4abt_frame_t,)
 
 if __name__ == "__main__":
     print("Main called okay.")
